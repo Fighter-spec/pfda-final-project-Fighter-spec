6dbfe86d7e07fc46eafcb7da9e09307a10d24e51
Jordan Andrews <jxa210059@utdallas.edu>
2025-05-10 20:42:15 -0500

    replaced hidden load message with accurate name

--- MindSweeper.py
+++ MindSweeper.py
@@ -117,9 +117,9 @@ def load_img(file):
 
 tile_types = {
     'empty':  load_img('Mine_Icon_dark 2.png'),
-    'mine':  load_img('Mine_Icon 2.png'),
+    'mine':  load_img('Mine_Icon_dark 2.png'),
     'warning': load_img('Mine_Icon_dark 3.png'),
-    '0': load_img('Mine_Icon_ 1.png'),
+    '0': load_img('Mine_Icon 1.png'),
     '1': load_img('Mine_Icon 3.png'),
     '2': load_img('Mine_Icon 4.png'),
     '3': load_img('Mine_Icon 5.png'),
@@ -127,11 +127,12 @@ tile_types = {
     '5': load_img('Mine_Icon 7.png'),
     '6': load_img('Mine_Icon 8.png'),
     '7': load_img('Mine_Icon 9.png'),
-    '8': load_img('Mine_Icon 10.png')
+    '8': load_img('Mine_Icon 10.png'),
+    'hidden': load_img('Mine_Icon 2.png')
  }
 
-for i in range(9):
-  tile_types[str(i)] = load_img(f'Mine_Icon_{i}.png')
+for i in range(1,9):
+  tile_types[str(i)] = load_img(f'Mine_Icon {i}.png')
 
 
 #Creating the Grid for the Game
