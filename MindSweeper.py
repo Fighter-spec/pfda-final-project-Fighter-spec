2fdcb4517efa798e73e68644c59e3fe49e6e3873
Jordan Andrews <jxa210059@utdallas.edu>
2025-05-10 21:59:10 -0500

    moved load_img and tile images above main

--- MindSweeper.py
+++ MindSweeper.py
@@ -14,6 +14,31 @@ Mine_Total = 10
 Background = (46, 45, 45)
 Grid = (255, 255 ,255)
 
+def load_img(file):
+   return pygame.transform.scale(pygame.image.load(file), (Tiles_Area, Tiles_Area))
+
+tile_types = {
+   'empty': load_img('Mine_Icon_dark 2.png'),
+   'mine': load_img('Mine_Icon_dark 1.png'),
+   'warning': load_img('Mine_Icon_dark 3.png'),
+   '0': load_img('Mine_Icon 1.png'),
+   '1': load_img('Mine_Icon 3.png'),
+   '2': load_img('Mine_Icon 4.png'),
+   '3': load_img('Mine_Icon 5.png'),
+   '4': load_img('Mine_Icon 6.png'),
+   '5': load_img('Mine_Icon 7.png'),
+   '6': load_img('Mine_Icon 8.png'),
+   '7': load_img('Mine_Icon 9.png'),
+   '8': load_img('Mine_Icon 10.png'),
+   'hidden': load_img('Mine_Icon 2.png')
+ 
+}
+
+
+
+
+
+
 def main():  
     pygame.init()
     screen = pygame.display.set_mode((Width, Height))
@@ -96,24 +121,7 @@ def main():
 
     pygame.display.update()
 
-def load_img(file):
-  return pygame.transform.scale(pygame.image.load(file), (Tiles_Area, Tiles_Area))
 
-tile_types = {
-    'empty':  load_img('Mine_Icon_dark 2.png'),
-    'mine':  load_img('Mine_Icon_dark 2.png'),
-    'warning': load_img('Mine_Icon_dark 3.png'),
-    '0': load_img('Mine_Icon 1.png'),
-    '1': load_img('Mine_Icon 3.png'),
-    '2': load_img('Mine_Icon 4.png'),
-    '3': load_img('Mine_Icon 5.png'),
-    '4': load_img('Mine_Icon 6.png'),
-    '5': load_img('Mine_Icon 7.png'),
-    '6': load_img('Mine_Icon 8.png'),
-    '7': load_img('Mine_Icon 9.png'),
-    '8': load_img('Mine_Icon 10.png'),
-    'hidden': load_img('Mine_Icon 2.png')
- }
 
 #for i in range(1,9):
   #tile_types[str(i)] = load_img(f'Mine_Icon {i}.png')
